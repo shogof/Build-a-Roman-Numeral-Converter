3a70f8fa78e04e9ee68cb324441e888e042a8eeb
Shegofa <shogofadeveloper12@gmail.com>
2024-06-29 12:37:17 +0400

    linters error sloved

--- script.js
+++ script.js
@@ -1,54 +1,53 @@
-document.getElementById('convert-btn').addEventListener('click', function () {
-  let numberInput = document.getElementById('number').value.trim();
-  let outPutE = document.getElementById('output');
+function convertToRoman(num) {
+    let romanNumeral = '';
+    const romanNumerals = [
+      { value: 1000, numeral: 'M' },
+      { value: 900, numeral: 'CM' },
+      { value: 500, numeral: 'D' },
+      { value: 400, numeral: 'CD' },
+      { value: 100, numeral: 'C' },
+      { value: 90, numeral: 'XC' },
+      { value: 50, numeral: 'L' },
+      { value: 40, numeral: 'XL' },
+      { value: 10, numeral: 'X' },
+      { value: 9, numeral: 'IX' },
+      { value: 5, numeral: 'V' },
+      { value: 4, numeral: 'IV' },
+      { value: 1, numeral: 'I' },
+    ];
+  
+    for (let i = 0; i < romanNumerals.length; i += 1) {
+      while (num >= romanNumerals[i].value) {
+        romanNumeral += romanNumerals[i].numeral;
+        num -= romanNumerals[i].value;
+      }
+    }
+  
+    return romanNumeral;
+  }
+  
+document.getElementById('convert-btn').addEventListener('click', () => {
+  const numberInput = document.getElementById('number').value.trim();
+  const outPutE = document.getElementById('output');
 
-  if (numberInput === '' || isNaN(numberInput)) {
+  if (numberInput === '' || Number.isNaN(numberInput)) {
     outPutE.textContent = 'Please enter a valid number.';
     return;
   }
 
-  let number = parseInt(numberInput, 10);
+  const number = parseInt(numberInput, 10);
 
   if (number < 1) {
-    outPutE.textContent =
-      'Please enter a number greater than or equal to 1.';
+    outPutE.textContent ='Please enter a number greater than or equal to 1.';
     return;
   }
 
   if (number > 3999) {
-    outPutE.textContent =
-      'Please enter a number less than or equal to 3999.';
+    outPutE.textContent ='Please enter a number less than or equal to 3999.';
     return;
   }
 
-  let romanNumeral = convertToRoman(number);
+  const romanNumeral = convertToRoman(number);
   outPutE.textContent = romanNumeral;
 });
 
-function convertToRoman(num) {
-  let romanNumeral = '';
-  let romanNumerals = [
-    { value: 1000, numeral: 'M' },
-    { value: 900, numeral: 'CM' },
-    { value: 500, numeral: 'D' },
-    { value: 400, numeral: 'CD' },
-    { value: 100, numeral: 'C' },
-    { value: 90, numeral: 'XC' },
-    { value: 50, numeral: 'L' },
-    { value: 40, numeral: 'XL' },
-    { value: 10, numeral: 'X' },
-    { value: 9, numeral: 'IX' },
-    { value: 5, numeral: 'V' },
-    { value: 4, numeral: 'IV' },
-    { value: 1, numeral: 'I' },
-  ];
-
-  for (let i = 0; i < romanNumerals.length; i += 1) {
-    while (num >= romanNumerals[i].value) {
-      romanNumeral += romanNumerals[i].numeral;
-      num -= romanNumerals[i].value;
-    }
-  }
-
-  return romanNumeral;
-}
