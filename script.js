7295520a5f96925697bc31ed29aeca4a8832787d
Shegofa <shogofadeveloper12@gmail.com>
2024-06-29 12:38:45 +0400

    linters error sloved

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-function convertToRoman(num) {
-    let romanNumeral = '';
-    const romanNumerals = [
-      { value: 1000, numeral: 'M' },
-      { value: 900, numeral: 'CM' },
-      { value: 500, numeral: 'D' },
-      { value: 400, numeral: 'CD' },
-      { value: 100, numeral: 'C' },
-      { value: 90, numeral: 'XC' },
-      { value: 50, numeral: 'L' },
-      { value: 40, numeral: 'XL' },
-      { value: 10, numeral: 'X' },
-      { value: 9, numeral: 'IX' },
-      { value: 5, numeral: 'V' },
-      { value: 4, numeral: 'IV' },
-      { value: 1, numeral: 'I' },
-    ];
-  
-    for (let i = 0; i < romanNumerals.length; i += 1) {
-      while (num >= romanNumerals[i].value) {
-        romanNumeral += romanNumerals[i].numeral;
-        num -= romanNumerals[i].value;
-      }
-    }
-  
-    return romanNumeral;
-  }
-  
 document.getElementById('convert-btn').addEventListener('click', () => {
   const numberInput = document.getElementById('number').value.trim();
   const outPutE = document.getElementById('output');
@@ -51,3 +23,30 @@ document.getElementById('convert-btn').addEventListener('click', () => {
   outPutE.textContent = romanNumeral;
 });
 
+function convertToRoman(num) {
+  let romanNumeral = '';
+  const romanNumerals = [
+    { value: 1000, numeral: 'M' },
+    { value: 900, numeral: 'CM' },
+    { value: 500, numeral: 'D' },
+    { value: 400, numeral: 'CD' },
+    { value: 100, numeral: 'C' },
+    { value: 90, numeral: 'XC' },
+    { value: 50, numeral: 'L' },
+    { value: 40, numeral: 'XL' },
+    { value: 10, numeral: 'X' },
+    { value: 9, numeral: 'IX' },
+    { value: 5, numeral: 'V' },
+    { value: 4, numeral: 'IV' },
+    { value: 1, numeral: 'I' },
+  ];
+
+  for (let i = 0; i < romanNumerals.length; i += 1) {
+    while (num >= romanNumerals[i].value) {
+      romanNumeral += romanNumerals[i].numeral;
+      num -= romanNumerals[i].value;
+    }
+  }
+
+  return romanNumeral;
+}
